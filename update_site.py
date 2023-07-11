7b8540bdc66f83bbea3cb833e74237bcbb112fe2
hanabi-league <139185531+hanabi-league@users.noreply.github.com>
2023-07-11 13:58:32 -0400

    Update update_site.py

--- update_site.py
+++ update_site.py
@@ -16,6 +16,7 @@ leaderboard = (
         .merge(player_data, hunted_scores, on='player_name', how='left')
         [[
             'player_name', 
+            'player_name_og',
             'player_rating', 
             'top_streak', 
             'hunted_scores'
@@ -25,11 +26,40 @@ leaderboard['player_rating'] = leaderboard['player_rating'].astype(int)
 
 players = player_data.sort_values('player_rating', ascending=False).to_dict('records')
 
-# Load the template from the filesystem
+
+# Create three separate DataFrames, each sorted by one of the metrics
+top_rating = leaderboard[['player_name', 'player_name_og', 'player_rating']].sort_values('player_rating', ascending=False)
+top_streak = leaderboard[['player_name', 'player_name_og', 'top_streak']].sort_values('top_streak', ascending=False)
+top_hunted_scores = leaderboard[['player_name', 'player_name_og', 'hunted_scores']].sort_values('hunted_scores', ascending=False)
+
+# Add a column to each DataFrame that contains the name of the metric
+top_rating['metric_name'] = 'player_rating'
+top_streak['metric_name'] = 'top_streak'
+top_hunted_scores['metric_name'] = 'hunted_scores'
+
+# Select the top player for each metric
+top_rating = top_rating.head(1)
+top_streak = top_streak.head(1)
+top_hunted_scores = top_hunted_scores.head(1)
+
+# Rename the metric column to 'metric_value'
+top_rating.rename(columns={'player_rating': 'metric_value'}, inplace=True)
+top_streak.rename(columns={'top_streak': 'metric_value'}, inplace=True)
+top_hunted_scores.rename(columns={'hunted_scores': 'metric_value'}, inplace=True)
+
+# Concatenate these three DataFrames to create the top_players DataFrame
+top_players = pd.concat([top_rating, top_streak, top_hunted_scores])
+
+# Sort the DataFrame by metric name (you can customize the sort order)
+sort_order = ['player_rating', 'top_streak', 'hunted_scores']  # update this list to your desired sort order
+top_players['metric_name'] = top_players['metric_name'].astype('category')
+top_players['metric_name'].cat.set_categories(sort_order, inplace=True)
+top_players.sort_values(['metric_name', 'metric_value'], ascending=[True, False], inplace=True)
+
+
+# Now you can pass top_players to the template
 env = Environment(loader=FileSystemLoader('.'))
 template = env.get_template('index.html')
-
-html = template.render(players=players)
-
+html = template.render(players=players, top_players=top_players.to_dict('records'))
 with open('index.html', 'w') as f:
     f.write(html)
